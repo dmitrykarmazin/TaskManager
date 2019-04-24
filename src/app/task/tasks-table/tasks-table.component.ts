300c3efc4509627f1f24c0934d1ff7817b53d7f8
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2019-04-24 18:00:44 +0300

    templ change,displaying list of task from service

--- src/app/task/tasks-table/tasks-table.component.ts
+++ src/app/task/tasks-table/tasks-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TaskService } from './../services/task.service';
+import { Subscription } from 'rxjs';
+import { Task } from './../models/task.model';
 
 @Component({
   selector: 'app-tasks-table',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-table.component.scss']
 })
 export class TasksTableComponent implements OnInit {
+  public active_tasks: Task[] = [];
+  subs: Subscription;
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.subs = this.taskService.getTasks()
+      .subscribe((tasks: Task[]) => this.active_tasks = tasks.filter(task => task.obj_status === 'active'));
   }
 
+  OnDestroy(): void {
+    if (this.subs) {
+      this.subs.unsubscribe();
+    }
+  }
 }
