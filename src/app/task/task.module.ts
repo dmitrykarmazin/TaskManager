506ead4b91a3228de74c5e3ff628a21e6129acbf
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2019-04-24 22:16:23 +0300

    update tepmlate,styles

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { TasksTableComponent } from './tasks-table/tasks-table.component';
 import { TasksDescriptionComponent } from './tasks-description/tasks-description.component';
 
@@ -10,7 +11,8 @@ import { TasksDescriptionComponent } from './tasks-description/tasks-description
   imports: [
     CommonModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ]
 })
 export class TaskModule { }
